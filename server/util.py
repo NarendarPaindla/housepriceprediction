f7c4171f6acac1e4c6fd9cc6b9441ff2f070b3d8
NarendarTrainer <narendartrainer@gmail.com>
2025-08-04 14:43:47 +0530

    loding files

--- server/util.py
+++ server/util.py
@@ -18,3 +18,27 @@ def get_estimated_price(location,sqft,bhk,bath):
     if loc_index>=0:
         x[loc_index]=1
     return round(__model.predict([x])[0],2)
+
+def load_saved_artifacts():
+    print("loading saved artifacts... start")
+    global  __data_columns
+    global  __locations
+
+    with open('./artifacts/columns.json','r') as f:
+        __data_columns=json.load(f)['data_columns']
+        __locations=__data_columns[3:]
+
+
+
+
+def get_location_names():
+    return __locations
+
+
+def get_data_columns():
+    return __data_columns
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_data_columns())
